c2c5a8ff4574854cbb2e8a344b020ada227b3b34
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-01-02 15:54:05 +0200

    add DB collection of stocks

--- public/js/stock.js
+++ public/js/stock.js
@@ -1,12 +1,15 @@
 /**
  * Created by Iaroslav Zhbankov on 02.01.2017.
  */
+
 var xmlHttp = new XMLHttpRequest();
 var url = 'http://localhost:3000/stock';
 xmlHttp.open("GET", url, false);
 xmlHttp.send(null);
 var dataSet = JSON.parse(xmlHttp.responseText);
 
+/*
+console.log(dataSet.dataset);
 var fullData = dataSet.dataset.data;
 var dataX = [];
 var dataY = [];
@@ -41,27 +44,6 @@ var data = {
             pointHitRadius: 10,
             data: dataY,
             spanGaps: false
-        }, {
-            label: "My Second dataset",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: dataY,
-            spanGaps: false
         }
     ]
 };
@@ -77,3 +59,10 @@ var myLineChart = new Chart(ctx, {
         }
     }
 });
+*/
+
+
+/*var xmlHttp = new XMLHttpRequest();
+var url = 'http://localhost:3000/stock/IBM/2014-01-01/2016-12-31';
+xmlHttp.open("GET", url, false);
+xmlHttp.send(null);*/
--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 var express = require('express');
 var app = express();
 var https = require('https');
+var MongoClient = require('mongodb').MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/stock_market';
 
 app.use('/', express.static('public'));
 
@@ -13,11 +15,44 @@ app.get('/', function (req, res) {
 });
 
 app.get('/stock', function (req, res) {
+
+    /*var apiKey = "zpY11hn5Z2iCF1n4hHwt";
+     var startDate = '2014-01-01';
+     var endDate = '2016-12-31';
+     var stockName = 'amzn';
+     var url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + stockName + '.json?column_index=4&start_date=' + startDate + '&end_date=' + endDate + '&collapse=monthly&api_key=' + apiKey;
+
+     https.get(url, function (response) {
+     var output = '';
+     response.setEncoding('utf8');
+     response.on('data', function (body) {
+     output += body;
+     });
+     response.on('end', function () {
+     var obj = JSON.parse(output);
+     res.send(obj);
+     });
+     });*/
+    MongoClient.connect(mongoUrl, function (err, db) {
+        db.collection('stocks').find().toArray(function (err, items) {
+            if (!items) {
+                db.close();
+                console.log("no stocks");
+                res.send(null);
+            } else {
+                console.log(items);
+                res.send(items);
+            }
+        });
+    });
+});
+
+app.get('/stock/:stockname/:start/:end', function (req, res) {
     var apiKey = "zpY11hn5Z2iCF1n4hHwt";
-    var startDate = '2014-01-01';
-    var endDate = '2014-12-31';
-    var stockName = 'IBM';
-    var url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + stockName + '.json?column_index=4&start_date=' + startDate + '&end_date=' + endDate + '&collapse=daily&transform=diff&api_key=' + apiKey;
+    var startDate = req.params.start; //'2014-01-01';
+    var endDate = req.params.end; //'2016-12-31';
+    var stockName = req.params.stockname;
+    var url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + stockName + '.json?column_index=4&start_date=' + startDate + '&end_date=' + endDate + '&collapse=monthly&api_key=' + apiKey;
     https.get(url, function (response) {
         var output = '';
         response.setEncoding('utf8');
@@ -26,7 +61,27 @@ app.get('/stock', function (req, res) {
         });
         response.on('end', function () {
             var obj = JSON.parse(output);
-            res.send(obj);
+            MongoClient.connect(mongoUrl, function (err, db) {
+                db.collection('stocks').findOne({"stockname": obj.dataset.dataset_code}, function (err, item) {
+                    if (item) {
+                        db.close();
+                        console.log("stock find");
+                        res.send(null)
+                    } else {
+                        db.collection('stocks').insertOne({
+                            "stockname": obj.dataset.dataset_code,
+                            "stockDescription": obj.dataset.description,
+                            "data": obj.dataset.data
+                        }, function (err, result) {
+                            if (!err) {
+                                console.log("stock added successfuly");
+                                db.close();
+                                res.send(null);
+                            }
+                        });
+                    }
+                });
+            });
         });
     });
 });
