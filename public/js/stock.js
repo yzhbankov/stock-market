ef93f551f0c91d44a7e3e9f84da44471b0b3f4b0
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-01-02 18:35:28 +0200

    improve layout

--- public/js/stock.js
+++ public/js/stock.js
@@ -1,6 +1,7 @@
 /**
  * Created by Iaroslav Zhbankov on 02.01.2017.
  */
+var colors = ['#FF5733', '#FFC733', '#83FF33', '#33FFF6', '#3393FF', '#5533FF', '#DD33FF', '#FF336E', '#FF3333','#33FF99', '#857347', '#47856F'];
 
 var xmlHttp = new XMLHttpRequest();
 var url = 'http://localhost:3000/stock';
@@ -40,24 +41,23 @@ var data = {
 };
 
 for (var i = 0; i < dataSet.length; i++) {
-
     data.datasets.push(
         {
             label: dataSet[i].stockname,
             fill: false,
             lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
+            backgroundColor: colors[i],
+            borderColor: colors[i],
             borderCapStyle: 'butt',
             borderDash: [],
             borderDashOffset: 0.0,
             borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
+            pointBorderColor: colors[i],
             pointBackgroundColor: "#fff",
             pointBorderWidth: 1,
             pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBackgroundColor: colors[i],
+            pointHoverBorderColor: colors[i],
             pointHoverBorderWidth: 2,
             pointRadius: 1,
             pointHitRadius: 10,
